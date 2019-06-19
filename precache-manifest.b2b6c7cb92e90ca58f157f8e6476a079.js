67352f01c36826b741d92912b9a858442309e50b
wellbye <7355233@qq.com>
2019-06-19 19:51:38 +0800

    Updates

---
+++ precache-manifest.b2b6c7cb92e90ca58f157f8e6476a079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5e0b42e11354da78836e1c00f5f45d",
+    "url": "/pnm/index.html"
+  },
+  {
+    "revision": "d0c834272ed22d902e84",
+    "url": "/pnm/static/css/2.9cfbc6ce.chunk.css"
+  },
+  {
+    "revision": "fc369f85ab8326523000",
+    "url": "/pnm/static/css/main.9915695b.chunk.css"
+  },
+  {
+    "revision": "d0c834272ed22d902e84",
+    "url": "/pnm/static/js/2.ec89b45f.chunk.js"
+  },
+  {
+    "revision": "fc369f85ab8326523000",
+    "url": "/pnm/static/js/main.d9128fb3.chunk.js"
+  },
+  {
+    "revision": "a2c471eb0badc037f4a5",
+    "url": "/pnm/static/js/runtime~main.74b69355.js"
+  },
+  {
+    "revision": "541539c235fbcf57b83fa64140ec1905",
+    "url": "/pnm/static/media/04.541539c2.jpg"
+  },
+  {
+    "revision": "29b498f6c64f5b91b1cde0d4de22aeba",
+    "url": "/pnm/static/media/iconfont.29b498f6.svg"
+  },
+  {
+    "revision": "3411b0f5ab39130010e463d09fdfe7e3",
+    "url": "/pnm/static/media/iconfont.3411b0f5.ttf"
+  },
+  {
+    "revision": "e00004747f8aad3926766334620e643a",
+    "url": "/pnm/static/media/iconfont.e0000474.eot"
+  }
+]);
